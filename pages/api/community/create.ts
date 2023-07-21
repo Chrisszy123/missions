a990f8faff8fd986b166922579da9e3918328ce8
micovi <micleaovidiul@gmail.com>
2023-07-21 17:02:23 +0300

    feat: multiple create page enhancements

--- pages/api/community/create.ts
+++ pages/api/community/create.ts
@@ -1,11 +1,12 @@
 import { createCommunity } from "models/community";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
-import authOptions  from "../auth/[...nextauth]";
+import authOptions from "../auth/[...nextauth]";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions);
   if (session) {
+    console.log("session", session)
     try {
       const data = req.body;
       const community = await createCommunity(data.communityData);
